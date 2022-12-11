07b967acc58a044da3e47a74c9f487bfc7e15139
Tralus <traoreabdoulaziz54@gmail.com>
2022-12-11 00:09:20 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        
+    }
+  
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,78 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';;
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//primeng
+import { ChipsModule } from 'primeng/chips';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { SliderModule } from 'primeng/slider';
+import { RippleModule } from 'primeng/ripple';
+import { DividerModule } from 'primeng/divider';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+
+import { MenubarModule } from 'primeng/menubar';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api'
+import { CalendarModule } from 'primeng/calendar';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { MenuModule } from 'primeng/menu';
+
+//new
+import {FieldsetModule} from 'primeng/fieldset';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MenubarModule,
+    HttpClientModule,
+    TableModule,
+    ToastModule,
+    ToolbarModule,
+    ButtonModule,
+
+    MultiSelectModule,
+    ContextMenuModule,
+    DropdownModule,
+    ProgressBarModule,
+    SliderModule,
+
+    ChipsModule,
+    DialogModule,
+    AutoCompleteModule,
+    ConfirmDialogModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RippleModule,
+
+   
+    DividerModule,
+    CalendarModule,
+    SpeedDialModule,
+    MenuModule,
+    FieldsetModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [MessageService, ConfirmationService, HttpClient]
+
 })
 export class AppModule { }
